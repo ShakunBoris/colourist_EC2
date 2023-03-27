ae9c73a54dec9be6485c1785e48c60368284d088
ShakunBoris <shakun.boris@gmail.com>
2023-03-27 19:44:39 +0400

    ah

--- colourist/settings.py
+++ colourist/settings.py
@@ -22,7 +22,7 @@ DEBUG = False if os.environ.get('DEBUG') == 'False' else True
 print(f'\n{20*"-"}ATENCIÓN! DEBUG={DEBUG}{20*"-"}\n')
 # print('environ', os.environ)
 if DEBUG == False:
-    print(DEBUG, os.environ.get('ALLOWED_HOSTS'), os.environ.get('ALLOWED_HOSTS').split(' '))
+    print(DEBUG, os.environ.get('ALLOWED_HOSTS'))
     ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 else:
     ALLOWED_HOSTS = ['*']
