39efdfa141abf18bb2941a660b2e9ef922bcd4ff
ShakunBoris <shakun.boris@gmail.com>
2023-03-29 14:09:18 +0400

    qwe

--- colourist/settings.py
+++ colourist/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-gii4k+gh&1u%qilw%#+udjw15p-mspdon$f@pw(@&_qyup89xg').strip("'")
 # DEBUG = False if os.environ.get('DEBUG') == 'False' else True
-DEBUG = False if str(os.getenv('DEBUG', 'True')).strip("'") == 'False' else True
+DEBUG = False if str(os.getenv('DEBUG', 'True')).lower == 'false' else True
 print(f'\n{20*"-"}ATENCIÓN! DEBUG={DEBUG}{20*"-"}\n')
 # print('environ', os.environ)
 if DEBUG == False:
