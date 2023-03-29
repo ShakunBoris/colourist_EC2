8191b7fa5f9bad96ec27e309af0029afedc153e1
ShakunBoris <shakun.boris@gmail.com>
2023-03-29 18:23:24 +0400

    asd

--- colourist/settings.py
+++ colourist/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-gii4k+gh&1u%qilw%#+ud
 # DEBUG = False if os.environ.get('DEBUG') == 'False' else True
 print('---------------------', str(os.getenv('DEBUG', 'True')).lower())
 # DEBUG = False if 'false' in str(os.getenv('DEBUG', 'True')).lower() else True
-DEBUG = os.environ.get('DEBUG', '') != 'False'
+DEBUG = os.environ.get('DEBUG', '').split() != 'False'
 # os.getenv("ENV_VAR", 'False').lower() in ('true', '1', 't')
 print(f'\n{20*"-"}ATENCIÓN! DEBUG={DEBUG} type={type(DEBUG)}{20*"-"}\n')
 # print('environ', os.environ)
